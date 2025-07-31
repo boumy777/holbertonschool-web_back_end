0073ac16a103a6eb92a2315bfe1c0238a47d2bf2
boumy777 <brandondialuta@gmail.com>
2025-07-31 14:17:42 +0200

    type-annotated function

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from typing import List, Union
+'''Module for sum_mixed_list function'''
+from typing import Union, List
+
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Sum a list of floats and integers and return the result'''
     return sum(mxd_lst)
-
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+'''Module for make_multiplier function'''
 from typing import Callable
 
-def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    return lambda x: x * multiplier
 
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''Return a function that multiplies a float by multiplier'''
+    def multiply(n: float) -> float:
+        '''Multiply a float by multiplier and return the result'''
+        return n * multiplier
+    return multiply
